9d619b4330a1cf02d51cbd68fe169c9340901484
Jakob Sterri <ajsterri@stud.ntnu.no>
2020-03-13 19:03:05 +0100

    Function for finding nearest neighbor. To slow, need sampling

---
+++ nearest_neighbor.py
@@ -0,0 +1,40 @@
+from plyfile import PlyData
+import numpy as np
+
+#Loading point clouds from file
+plydata = PlyData.read('test.ply')
+x_points = plydata['vertex']['x']
+y_points = plydata['vertex']['y']
+z_points = plydata['vertex']['z']
+p = np.array([x_points, y_points, z_points])
+
+plydata = PlyData.read('test2.ply')
+x_points = plydata['vertex']['x']
+y_points = plydata['vertex']['y']
+z_points = plydata['vertex']['z']
+
+q = np.array([x_points, y_points, z_points])
+
+#Transposing clouds
+p_new = p[0:10, 0:10].T
+q_new = q[0:10, 0:10].T
+
+#Comparing each point in one point cloud to all in other
+def nearest_point(P, Q):
+    dis = np.zeros(P.shape[0]) # Dist is array of distances
+    index = np.zeros(Q.shape[0], dtype = np.int)
+    for i in range(P.shape[0]):
+        minDis = np.inf #Initially minimum distance = infinity
+        for j in range(Q.shape[0]):
+            tmp = np.linalg.norm(P[i] - Q[j], ord = 2) #Fast way to calculate distance
+            if minDis > tmp: 
+                minDis = tmp
+                index[i] = j
+        dis[i] = minDis
+    return dis, index
+
+dis, index = nearest_point(p_new, q_new)
+print(dis, index)
+
+
+
